3f880964ab0517db1a69687850ca6ea4851a0502
sahara <saharina123y@gmail.com>
2025-08-12 11:43:05 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// Code your solution in this file!
 
-// Write your code here
+// Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // should equal 62
+
+// Random number greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Modulus (remainder is 4)
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4; // remainder is 4
+
+// Maximum number in a set
+const max = Math.max(1, 5, 10, 15, 20);
